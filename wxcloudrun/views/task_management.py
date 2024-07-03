04fbf7bb03894a73802da1217a75503356b3ee04
JlikSenior <jliksenior@gmail.com>
2024-07-04 01:43:21 +0800

    添加任务管理

--- wxcloudrun/views/task_management.py
+++ wxcloudrun/views/task_management.py
@@ -4,3 +4,11 @@ from . import bp
 @bp.route('/task-management')
 def task_management():
     return render_template('task_management/task_management.html')
+
+@bp.route('/create-task')
+def create_task():
+    return render_template('task_management/create_task.html')
+
+@bp.route('/task-list')
+def task_list():
+    return render_template('task_management/task_list.html')
